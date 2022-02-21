3992220c045fed97279e32139032d84d8dd6936f
krupalvora <krupal.vora@sakec.ac.in>
2022-02-21 19:16:54 +0530

    added dynamic url with 1 min timer

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import kv_mail
-
+from itsdangerous import URLSafeTimedSerializer,SignatureExpired
 app = Flask(__name__)
-
+s=URLSafeTimedSerializer('Thisisasecret!')
 
 @app.route('/')
 def home():
@@ -13,23 +13,28 @@ def home():
 def rfid(rfid):
     # generate url mail to specific user
     hash = 'adahgl09-9dgdfg-dbd-fg9-34'  # demo hash
-    url = 'http://localhost:5000/verified_rfid/' + hash
-    body = 'click here to login: ' + url
+    token=s.dumps(rfid, salt='rfid')
+    body = 'click here to login: ' + url_for('verified_rfid', token=token, _external=True)
     user_email = 'krupal.vora@sakec.ac.in'
     kv_mail.mail('mg9417054@gmail.com', 'eHi3mohwier', user_email, "Verify your password", body)
     return 'Hello world!'
 
 
-@app.route('/verified_rfid/<url>')
-def verified_rfid(url):
+@app.route('/verified_rfid/<token>')
+def verified_rfid(token):
+    try:
+        rfid=s.loads(token, salt='rfid', max_age=100)
+    except SignatureExpired:
+        return '<h1>session expired</h1>'
     # open unique url and ask for inputs
-    return render_template('index.html')
+    return render_template('index.html',rfid=rfid)
 
 
 @app.route('/verify', methods=('GET', 'POST'))
 def verify():
     if request.method == 'POST':
         pswd = request.form["pswd"]
+        rfid = request.form["rfid"]
         # check username and password
         return 'door opened'
     else:
